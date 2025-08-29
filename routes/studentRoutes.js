50a545364783f5a81c13cec463e6cc27bb23c5c0
123ol <kratosolomon25@gmail.com>
2025-08-29 16:55:52 +0100

    new update for progress2

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -3,8 +3,8 @@ import { protect } from '../middleware/authMiddleware.js';
 import { getEnrolledStudents, getTotalStudents, deleteStudent } from '../controllers/userController.js';
 
 const router = express.Router();
-router.get('/students/enrolled', protect, getEnrolledStudents);
-router.get('/students', protect, getTotalStudents);
-router.delete('/students/:id', protect, deleteStudent);
+router.get('/api/students/enrolled', protect, getEnrolledStudents);
+router.get('/api/students', protect, getTotalStudents);
+router.delete('/api/students/:id', protect, deleteStudent);
 
 export default router;
